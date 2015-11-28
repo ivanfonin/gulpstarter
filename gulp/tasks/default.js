b7774b94a850f6dab17de54f384cc0d6c8c1b6a1
Ivan Fonin <mail@ivanfonin.com>
2015-11-28 13:26:52 +0300

    Created basic task as example

---
+++ gulp/tasks/default.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    config = require('../config.js');
+
+gulp.task('default', function() {
+	
+	console.log('Project source directory is: ' + config.project.src);
+
+});
